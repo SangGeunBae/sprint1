a4f3a37fb06c782781f95985c3d8bf6fb803ce72
SangGeunBae <sgb719@gmail.com>
2021-01-19 16:50:52 +0900

    final

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -1,3 +1,3 @@
-insert into posts (title, author, content, created_date, modified_date) values ('배상근', '배상근@gmail.com', '테스트1의 본문', now(), now());
-insert into posts (title, author, content, created_date, modified_date) values ('김나연', '김나연@gmail.com', '테스트2의 본문', now(), now());
-insert into posts (title, author, content, created_date, modified_date) values ('최기용', '최기용@gmail.com', '테스트3의 본문', now(), now());
+insert into posts (title, author, content, created_date, modified_date) values ('배상근', '배상근@lgcns.com', '테스트1의 본문', now(), now());
+insert into posts (title, author, content, created_date, modified_date) values ('김나연', '김나연@lgcns.com', '테스트2의 본문', now(), now());
+insert into posts (title, author, content, created_date, modified_date) values ('최기용', '최기용@lgcns.com', '테스트3의 본문', now(), now());
