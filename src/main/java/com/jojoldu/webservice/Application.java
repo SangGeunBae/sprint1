52747984f84fd4a2ff626c78c3fac71b88792125
SangGeunBae <sgb719@gmail.com>
2021-01-22 12:05:32 +0900

    no answer

--- src/main/java/com/jojoldu/webservice/Application.java
+++ src/main/java/com/jojoldu/webservice/Application.java
@@ -10,8 +10,8 @@ public class Application {
 
 	public static final String APPLICATION_LOCATIONS = "spring.config.location="
 			+ "classpath:application.yml,"
-			+ "/home/ec2-user/app/config/springboot-webservice/real-application.yml";
-			//+ "C:/Users/SG/Desktop/app/config/springboot-webservice/real-application.yml";
+			+ "/app/config/springboot-webservice/real-application.yml,"
+			+ "C:/Users/SG/Desktop/app/config/springboot-webservice/real-application.yml";
 
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(Application.class)
